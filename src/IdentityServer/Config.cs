9e0cedf9498eb585544259cf7df1d0a83262bc8a
Satoshi Iwao <stsiwo@gmail.com>
2019-05-25 12:28:51 -0700

    added sln and IdentityServer4 template project

---
+++ src/IdentityServer/Config.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+using IdentityServer4.Test;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId()
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new ApiResource[] { };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new Client[] { };
+        }
+
+        public static IEnumerable<TestUser> GetTestUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "alice",
+                    Password = "password"
+                }
+            };
+        }
+    }
+}
