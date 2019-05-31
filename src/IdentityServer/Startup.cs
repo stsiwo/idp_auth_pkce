8057e06f2dfc74bff01654af8c9bdb93bb24b30a
Satoshi Iwao <stsiwo@gmail.com>
2019-05-31 04:58:45 -0700

    added identityserver4.entityframework for necessary tables (except for Users)

--- src/IdentityServer/Startup.cs
+++ src/IdentityServer/Startup.cs
@@ -3,34 +3,58 @@
 
 
 using System;
+using System.Linq;
+using System.Reflection;
 using IdentityServer.Infrastracture;
 using IdentityServer4.Configuration;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace IdentityServer
 {
     public class Startup
     {
         public IHostingEnvironment Environment { get; }
+        private readonly ILogger<Startup> _logger;
 
-        public Startup(IHostingEnvironment environment)
+        public Startup(IHostingEnvironment environment, ILogger<Startup> logger)
         {
             Environment = environment;
+            _logger = logger;
         }
 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
 
+            const string connectionString = @"Data Source=(LocalDb)\MSSQLLocalDB;database=IdentityServer4.Quickstart.EntityFramework-2.0.0;trusted_connection=yes;";
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
             var builder = services.AddIdentityServer()
-                .AddInMemoryIdentityResources(Config.GetIdentityResources())
-                .AddInMemoryApiResources(Config.GetApis())
-                .AddInMemoryClients(Config.GetClients())
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = b =>
+                        b.UseSqlServer(connectionString,
+                            sql => sql.MigrationsAssembly(migrationsAssembly));
+
+                })
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = b =>
+                        b.UseSqlServer(connectionString,
+                        sql => sql.MigrationsAssembly(migrationsAssembly));
+
+                    // this enables automatic token cleanup. this is optional.
+                    options.EnableTokenCleanup = true;
+                })
                 .AddTestUsers(Config.GetTestUsers());
+
 
             services.Configure<RazorViewEngineOptions>(options => 
             {
@@ -69,5 +93,6 @@ namespace IdentityServer
 
             app.UseMvcWithDefaultRoute();
         }
+
     }
 }
