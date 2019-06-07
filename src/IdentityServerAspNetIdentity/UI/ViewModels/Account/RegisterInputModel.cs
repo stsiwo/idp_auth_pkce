aafdb5f5bb61926ce80b9adaf5afda72c9ce9ede
Satoshi Iwao <stsiwo@gmail.com>
2019-06-07 02:56:45 -0700

    added Register Feature to IdentityServer4 (change Razor Page to MVC since there does not seem no way to mix those two)

---
+++ src/IdentityServerAspNetIdentity/UI/ViewModels/Account/RegisterInputModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerAspNetIdentity.UI.ViewModels.Account
+{
+    public class RegisterInputModel
+    {
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; } = null;
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; } = null;
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; } = null;
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; } = null;
+
+    }
+}
---
+++ src/IdentityServerAspNetIdentity/UI/ViewModels/Account/RegisterViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerAspNetIdentity.UI.ViewModels.Account
+{
+    public class RegisterViewModel : RegisterInputModel
+    {
+        public RegisterViewModel(string returnUrl)
+        {
+            ReturnUrl = returnUrl;
+        }
+
+        public string ReturnUrl { get; set; } = null;
+    }
+}
